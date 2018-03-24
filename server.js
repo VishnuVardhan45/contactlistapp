a493ca6b04d95f865e13499676711c967666ce0a
Yeddula <yeddulav@ds.ad.adp.com>
2018-03-24 19:56:41 +0530

    angular integrated release

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-res.send("hello");
-}
-);
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("server running on port 3000");
